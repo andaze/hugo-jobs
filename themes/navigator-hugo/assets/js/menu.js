cbecf3da7bfb741b2618e806059c1081b3149891
bhavya <bhavya.ameio@gmail.com>
2021-03-16 12:32:01 +0530

    home slider Change

--- themes/navigator-hugo/assets/js/menu.js
+++ themes/navigator-hugo/assets/js/menu.js
@@ -5,3 +5,10 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+$('.example-1').square1({
+  caption: 'none',
+  theme: 'light',
+  lazy_load: true
+
+});
