318712cc26be61767e34d92c1990bdfaca8dd07a
bhavya <bhavya.ameio@gmail.com>
2021-04-13 13:00:05 +0530

    changes

--- postcommand.sh
+++ postcommand.sh
@@ -8,5 +8,8 @@ sudo rm -rf /home/dameio/public_html/hugo-baigie/deploy/public
 echo
 sudo ./hugo -d deploy/public
 echo
+git add .
+git commit -m'update code'
+git push origin master
 #cd /home/dameio/public_html/hugo-baigie
 #mkdir ashish
